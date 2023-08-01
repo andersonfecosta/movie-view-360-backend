2ecf40a7c764d747abb48cea27b8cd9ae4f4ce9f
Anderson Costa <andersonfelipedc@gmail.com>
2023-08-01 13:30:29 -0300

    first commit

---
+++ src/main/java/com/movieview360/movieview360/MovieView360Application.java
@@ -0,0 +1,13 @@
+package com.movieview360.movieview360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieView360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieView360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieview360/movieview360/MovieView360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieview360.movieview360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieView360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
