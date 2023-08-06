0c5d397330d5156c71acf4cfa8ee2c7a80670f9c
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2023-08-06 18:33:57 -0300

    add mapping allowed origins local

--- src/main/java/com/movieview360/movieview360/MovieView360Application.java
+++ src/main/java/com/movieview360/movieview360/MovieView360Application.java
@@ -1,13 +1,30 @@
 package com.movieview360.movieview360;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Movie View 360 Backend",
+		description = "Aplicação web estilo Netflix, backend desenvolvido em Spring Boot e Java",
+		version = "1.0.0"))
 public class MovieView360Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieView360Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+			}
+		};
+	}
 }
