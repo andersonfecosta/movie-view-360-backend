0363d498f7e8d8cb462452e83048b393a7194997
Anderson Costa <andersonfelipedc@gmail.com>
2023-08-14 20:23:37 -0300

    Criação SecurityConfig; adição dependências

---
+++ src/main/java/com/movieview360/movieview360/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.movieview360.movieview360.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/public/**").permitAll()
+                                .antMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
